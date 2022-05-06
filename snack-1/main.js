0de40419685d1606e8a9874a2ce8c90099ac1109
EttoreBarcella <ettore.barcella@gmail.com>
2022-05-06 13:58:58 +0200

    first commit

---
+++ snack-1/main.js
@@ -0,0 +1,5 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// chiedo all'utente di inserire una parola
+const word = prompt("inserisci una parola")
