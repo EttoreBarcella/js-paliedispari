c1921134c7a43eda3e0a4aaf06ad2cca1c439d59
EttoreBarcella <ettore.barcella@gmail.com>
2022-05-09 09:31:01 +0200

    forgot to push second snack

--- snack-2/main.js
+++ snack-2/main.js
@@ -4,3 +4,35 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
+
+// funzione random number 
+function rrandomNum (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+// funzione se è pari
+function isEven(num){
+    return num%2===0;
+}
+
+
+
+let userWin;
+do{
+    userWin = prompt("Pari o Dispari?");
+    userWin = userWin.toLocaleLowerCase();
+} while( userWin !== "pari" &&  userWin !== "dispari")
+
+let userNumber;
+do{
+    userNumber = Number(prompt("inserisci un numero tra 1 e 5!"));
+} while((userNumber<1 || userNumber>5) || isNaN(userNumber)) 
+
+const sum = userNumber + rndRangeNumber(1,6);
+console.log(userWin,userNumber, numCPU, sum);
+
+if((isEven(sum) && userWin==="pari") || (!isEven(sum) && userWin==="dispari")){ 
+    console.log("hai vinto!");
+} else { 
+    console.log("hai perso!");
+}
